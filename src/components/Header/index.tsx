308f65ea70c2649c6ea41c559fc326c7dd5f00fb
mohmmedalkhatim <136941730+mohmmedalkhatim@users.noreply.github.com>
2025-03-03 16:04:05 +0200

    adding the navbar and header commponent

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import { AppShellHeader } from "@mantine/core"
+
+
+function Header() {
+  return (
+    <AppShellHeader>
+        header
+    </AppShellHeader>
+  )
+}
+export default Header
---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import { AppShellNavbar } from "@mantine/core"
+
+
+function Navbar() {
+  return (
+    <AppShellNavbar>
+        Navbar
+    </AppShellNavbar>
+  )
+}
+export default Navbar
